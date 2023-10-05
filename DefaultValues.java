f2ae1b980f3cb092bc188ec2e86eaa21d748e6c9
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 08:26:40 +0530

    Added :Display Message

---
+++ DefaultValues.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+/*
+      1. Start
+      2. Declare variables for each primitive data type: byte, short, int,long, float, double, char, boolean.
+      3. Print the default values of each variable.
+      4. End
+ */
+
+public class DefaultValues {
+    static byte defaultValueByte;
+    static short defaultValueShort;
+    static int defaultValueInt;
+    static long defaultValueLong;
+    static float defaultValueFloat;
+    static double defaultValueDouble;
+    static char defaultValueChar;
+    static boolean defaultValueBoolean;
+    public static void main(String[] args) {
+        System.out.println("Default Value of byte: " + defaultValueByte);
+        System.out.println("Default Value of short: " + defaultValueShort);
+        System.out.println("Default Value of int: " + defaultValueInt);
+        System.out.println("Default Value of long: " + defaultValueLong);
+        System.out.println("Default Value of float: " + defaultValueFloat);
+        System.out.println("Default Value of double: " + defaultValueDouble);
+        System.out.println("Default Value of char: " + defaultValueChar);
+        System.out.println("Default Value of boolean: " + defaultValueBoolean);
+    }
+}
---
+++ DisplayMessage.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+/*
+     1.Start
+     2.Declare variable message.
+     3.Initialize a variable "message" with the desired message to be displayed.
+     4.Print the value of "message" to the console.
+     5.End
+ */
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        System.out.println(message);
+    }
+}
